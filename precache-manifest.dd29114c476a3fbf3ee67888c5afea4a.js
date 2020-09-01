47b77b051d127c806b233e163baabb8a675946c9
Sandro Brayen <sandro.brayen@shopee.com>
2020-09-01 15:21:12 +0700

    Updates

---
+++ precache-manifest.dd29114c476a3fbf3ee67888c5afea4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcb78d4eb50f30781348ca75cb0f3b1",
+    "url": "/anjay-generator/index.html"
+  },
+  {
+    "revision": "7e2c7d569ca064de088d",
+    "url": "/anjay-generator/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "1daecbce3caec1cef6f2",
+    "url": "/anjay-generator/static/css/main.bcdcf28e.chunk.css"
+  },
+  {
+    "revision": "7e2c7d569ca064de088d",
+    "url": "/anjay-generator/static/js/2.b112b839.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/anjay-generator/static/js/2.b112b839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1daecbce3caec1cef6f2",
+    "url": "/anjay-generator/static/js/main.66a59390.chunk.js"
+  },
+  {
+    "revision": "7671eb872c154c47ef61",
+    "url": "/anjay-generator/static/js/runtime-main.4897c4da.js"
+  }
+]);
